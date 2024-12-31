45e7d155f1ab4685e5f21d951a9b4cf8f8ccec4e
Neal Magee <nem2p@virginia.edu>
2024-12-31 16:15:22 -0500

    Stripping down

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+/Users/nmagee/.rubies/ruby-3.3.5/bin/bundle exec jekyll build
 /Users/nmagee/.local/bin/aws s3 sync _site s3://uvasds.sh/
 /Users/nmagee/.local/bin/aws cloudfront create-invalidation --distribution-id E24IRLC86TMAXP --path "/*"
